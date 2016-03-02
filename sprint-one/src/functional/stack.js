00d362f3cfb051c048d56f31a702563fa2f9a2db
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-02 14:48:04 -0800

    Implement stack data structure using functional pattern, and add additional test to stack shared behavior

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[items] = value;
+    items++;
   };
 
   someInstance.pop = function() {
+    items--;
+    if (items < 0) {
+      items = 0;
+    }
+    return storage[items];
   };
 
   someInstance.size = function() {
+    return items;
   };
 
   return someInstance;
