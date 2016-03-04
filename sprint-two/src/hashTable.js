8c37d8d2194b00c837ff0eea262248a873101cd3
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-04 15:52:21 -0800

    Fix issue where duplicate key value pairs were being created in hash table buckets

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,32 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var subIndex, bucket, insertIndex;
+
   if (typeof this._storage.get(index) === 'undefined') {
-    var bucket = LimitedArray(8);
+    bucket = LimitedArray(8);
     bucket.set(0, 1);
     this._storage.set(index, bucket);
   } else {
-    var bucket = this._storage.get(index);
+    bucket = this._storage.get(index);
   }
+
+  subIndex = bucket.get(0);
+  insertIndex = subIndex;
 
   bucket.each(function(item, index, arr) {
     if (item === k && (index + 1) % 2 === 0) {
-      arr[index + 1] = v;
+      insertIndex = index;
     } 
   });
 
-
-  // for (var i = 1; i < bucket.get(0); i += 2) {
-  //   if (bucket.get(i) === k) {
-  //     bucket.set(i + 1, v);
-  //     return;
-  //   }
-  // }
-
-  var subIndex = bucket.get(0);
-  bucket.set(subIndex, k);
-  bucket.set(subIndex + 1, v);
-  bucket.set(0, subIndex + 2); 
+  bucket.set(insertIndex, k);
+  bucket.set(insertIndex + 1, v);
+  bucket.set(0, subIndex + 2);
+   
 };
 
 HashTable.prototype.retrieve = function(k) {
