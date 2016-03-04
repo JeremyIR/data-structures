a8a58d57fe0fae054fdf3cbaed85a773d4ba31a5
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-04 11:23:07 -0800

    Partially implement binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,30 @@
 var BinarySearchTree = function(value) {
+
+  var someInstance = Object.create(BSTMethods);
+  someInstance.left = null;
+  someInstance.right = null;
+  someInstance.value = value;
+
+  return someInstance;
+
 };
+
+var BSTMethods = {};
+
+BSTMethods.insert = function(value) {
+  if (this.value > value) {
+    this.left = value;
+  } else {
+    this.right = value;
+  }
+};
+
+BSTMethods.contains = function() {};
+
+BSTMethods.depthFirstLog = function() {};
+
+
+
 
 
 /*
