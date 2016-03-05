5d0e918c5eee29b1e918209504c54f89373af4eb
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-05 09:54:35 -0800

    Implement binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,15 +13,58 @@ var BSTMethods = {};
 
 BSTMethods.insert = function(value) {
   if (this.value > value) {
-    this.left = value;
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
   } else {
-    this.right = value;
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
   }
 };
 
-BSTMethods.contains = function() {};
+BSTMethods.contains = function(target) {
 
-BSTMethods.depthFirstLog = function() {};
+  if (this.value === target) {
+    return true;
+  }
+
+  if (target < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(target);
+    }
+  }
+
+  if (target > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(target);
+    }
+  }
+
+};
+
+BSTMethods.depthFirstLog = function(cb) {
+
+  cb(this.value);
+
+
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
+
+};
 
 
 
