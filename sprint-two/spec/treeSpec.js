da3ada6d549e5a1a3661839939621d6cd119c224
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-05 11:42:26 -0800

    Add new tests for .parent and .removeFromParent for tree data structure

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -2,18 +2,36 @@ describe('tree', function() {
   var tree;
 
   beforeEach(function() {
-    tree = Tree();
+    tree = Tree(13);
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "contains" and "removeFromParent",and a properties named "value", "parent, and "children"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
+    expect(tree.hasOwnProperty('children')).to.equal(true);
   });
 
   it('should add children to the tree', function() {
     tree.addChild(5);
     expect(tree.children[0].value).to.equal(5);
+  });
+
+  it('should add a child which points to its parent', function() {
+    tree.addChild(2);
+    expect(tree.children[0].parent.value).to.equal(13);
+  }); 
+
+  it('should separate a parent tree from a child tree', function() {
+    tree.add.child(2);
+    tree.add.child(3);
+    tree.children[0].addChild(7);
+    var subTree = tree.children[0].removeFromParent();
+    expect(subTree.value).to.equal(2);
+    expect(subTree.children[0]).to.equal(7);
+    expect(tree.children[0].value).to.equal(3);
   });
 
   it('should return true for a value that the tree contains', function() {
